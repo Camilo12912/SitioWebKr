6413fa9eefa37a54c359fe7bb15ce8a046c7fd09
CamiloGay <lilianapallares129@gmail.com>
2023-08-24 09:54:53 -0500

    Primer commit, Sitio web completo

---
+++ Js/Funciones2.js
@@ -0,0 +1,20 @@
+window.onscroll = function() {
+    scrollFunction();
+  };
+  
+  function scrollFunction() {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+      document.getElementById("navbar").style.backgroundColor = "#F5CBA7";
+      document.getElementById("navbar").style.padding = "10px";
+      document.getElementById("navbar-right").style.paddingTop = "10px";
+      document.getElementById("logo").style.paddingLeft = "10px";
+      document.getElementById("imagen").style.width = "140px"; // 
+    } else {
+        
+      document.getElementById("navbar").style.backgroundColor = "#F5CBA7";
+      
+      document.getElementById("navbar").style.padding = "20px 20px";
+      document.getElementById("imagen").style.width = "300px"; 
+      document.getElementById("navbar-right").style.paddingTop = "20px";
+    }
+  }
